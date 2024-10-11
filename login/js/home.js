779959e9d45e6f9ec626f590746241a540e1b7f7
Débora Serra <debora.r.serra@gmail.com>
2024-10-10 17:56:34 -0700

    feat: add login

--- login/js/home.js
+++ login/js/home.js
@@ -11,13 +11,24 @@ Function onAuthStateChanged(user)
 Write a function to check if a user is logged
 */
 
+const onAuthStateChanged = (user) => {
+};
 
-window.addEventListener('load', function () {
-
-    //Listen for auth state changes
-    firebase.auth().onAuthStateChanged(onAuthStateChanged);
-
-    //Implement SignOut Function
-
-
+window.addEventListener("load", function () {
+  //Listen for auth state changes
+  firebase.auth().onAuthStateChanged(onAuthStateChanged);
+  //Implement SignOut Function
+  document.querySelector("#sign-out-button").addEventListener("click", () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        console.log("Logged out");
+        alert("Logged out");
+        location.href = "index.html";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 });
--- login/js/main.js
+++ login/js/main.js
@@ -11,45 +11,68 @@ Function onAuthStateChanged(user)
 Write a function to check if a user is logged
 */
 
+window.addEventListener("load", function () {
+  //Listen for auth state changes
 
-window.addEventListener('load', function () {
+  document
+    .getElementById("sign-in-button")
+    .addEventListener("click", function () {
+      let provider = new firebase.auth.GoogleAuthProvider();
 
-    //Listen for auth state changes
-
-    document.getElementById('sign-in-button').addEventListener('click', function () {
-
-        let provider = new firebase.auth.GoogleAuthProvider();
-
-        provider.addScope('email');
-        firebase.auth().signInWithPopup(provider)
-            .then(function (result) {
-                console.log('Logging sucessfully', result.user);
-                location.href = 'home.html';
-            })
-            .catch(function (error) {
-                console.log('Logging fail', error);
-            });
+      provider.addScope("email");
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(function (result) {
+          console.log("Logging sucessfully", result.user);
+          location.href = "home.html";
+        })
+        .catch(function (error) {
+          console.log("Logging fail", error);
+        });
     });
 
-    document.getElementById('sign-in-2').addEventListener('click', function () {
+  document.getElementById("sign-in-2").addEventListener("click", function () {
+    let emailTxt = document.getElementById("email").value;
+    let passtxt = document.getElementById("password").value;
 
-        let emailTxt = document.getElementById('email').value;
-        let passtxt = document.getElementById('password').value;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailTxt, passtxt)
+      .then((userCredential) => {
+        // Signed in
+        let user = userCredential.user;
+        // ...
+        console.log("Logging sucessfully");
+        alert("Logging sucessfully");
+        location.href = "home.html";
+      })
+      .catch((error) => {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        console.log("Logging fail", errorMessage);
+      });
+  });
 
-        firebase.auth().signInWithEmailAndPassword(emailTxt, passtxt)
-            .then((userCredential) => {
-                // Signed in
-                let user = userCredential.user;
-                // ...
-                console.log('Logging sucessfully');
-                alert('Logging sucessfully');
-                location.href = 'home.html';
-            })
-            .catch((error) => {
-                let errorCode = error.code;
-                let errorMessage = error.message;
-                console.log('Logging fail', errorMessage);
-            });
+  document.getElementById("sign-in-3").addEventListener("click", function () {
+    let emailTxt = document.getElementById("email").value;
+    let passtxt = document.getElementById("password").value;
 
-    });
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailTxt, passtxt)
+      .then((userCredential) => {
+        // Signed in
+        let user = userCredential.user;
+        // ...
+        console.log("Account created");
+        alert("Account created");
+        location.href = "home.html";
+      })
+      .catch((error) => {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        console.log("Logging fail", errorMessage);
+      });
+  });
 });
